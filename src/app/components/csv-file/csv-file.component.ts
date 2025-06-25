2b7588a7fa0d3364d45878845bd03356c4cf25f2
Chris2240 <kris2240.kp@gmail.com>
2025-06-25 21:13:25 +0100

    implementation DbService, csv-file.component.ts and updating the csv-file component.html as well

--- src/app/components/csv-file/csv-file.component.ts
+++ src/app/components/csv-file/csv-file.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import Papa from 'papaparse';
+import { DbService } from '../../services/db.service';
+
 
 @Component({
   selector: 'app-csv-file',
@@ -6,5 +9,73 @@ import { Component } from '@angular/core';
   styleUrl: './csv-file.component.css'
 })
 export class CsvFileComponent {
+
+  constructor(private service: DbService) {}  // Angular will manage the lifecycle and dependency injection for me.
+
+  // converting csv file into jason file using papaprse library
+  private readCSVFileAndConvertToJSON(csvFile: File): Promise<any[]> {   // "File" is the standard type for file inputs in browser
+    
+    const readCSVFileAndConvertToJSON = new Promise<any[]>((resolve, reject) => {
+
+        const fileReader = new FileReader();
+
+        fileReader.addEventListener("load", ev => {
+            const csv = ev.target?.result as string;    // since FileReader.result can also be ArrayBuffer or null. Adding "as string" ensures TypeScript understands the expected type
+
+            Papa.parse(csv, {
+                header: true,           // treat the first row as headers
+                skipEmptyLines: true,   // skip empty lines
+                dynamicTyping: true,    // this means every numbers or boolean values which are in string it will automatically converts to: numbers string into numbers and "true" or "false" strings into boolean true or false values
+                                        // by default ("dynamicTyping: flase"), all fields remains string only
+                complete: (result: Papa.ParseResult<any>) => {   // treat as when task is complete successful
+                    resolve(result.data);
+                    console.log(result.data); // displaying all database objects in console
+                },
+                error: (error: Error) => {
+                    reject(error);
+                }
+            });
+        });
+
+        
+        fileReader.addEventListener("error", () => {
+            reject(fileReader.error);
+        });
+
+        fileReader.readAsText(csvFile);
+    });
+
+    return readCSVFileAndConvertToJSON;
+  };
+
+
+  // Submit button
+  async submitButton(): Promise<void>{
+    const csvInputFile = document.getElementById('input-file-file-load') as HTMLInputElement | null;
+
+        if(!csvInputFile){
+            console.error("The \"input-file-file-load\" element not found.");
+            return;
+        }
+
+        const file = csvInputFile.files?.[0];
+
+        if (file) {
+
+            // The try-catch block contains the complex asynchronous operations
+            try {
+                const data = await this.readCSVFileAndConvertToJSON(file);
+
+                // Clearing IDB store if we need to rewrite / update data
+                // await ClearCsvIDBStore();
+
+                // Restore IDB data
+                await this.service.storeCSVJSONDataInIDB(data);
+            }
+            catch (error) {
+                console.log(`Error: ${error}`);
+            }
+        } 
+  }
 
 }
--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -5,5 +5,86 @@ import { Injectable } from '@angular/core';
 })
 export class DbService {
 
-  constructor() { }
+  // constructor() { }
+
+  // Open IDB(IndexedDB) and creating tables in new CSV database
+  initCSVIndexedDB(): Promise<IDBDatabase> {
+
+      let db: IDBDatabase;    // declare db with the appropriate(odpowiedni) type
+
+      const initIDB = new Promise<IDBDatabase>((resolve, reject) => {
+
+          // opening database
+          const openRequestCSVdb = window.indexedDB.open('CSVDataIDB', 1);
+
+          // creating tables of new opened databases
+          openRequestCSVdb.addEventListener("upgradeneeded", (ev: IDBVersionChangeEvent) => {
+              const request = ev.target as IDBOpenDBRequest;  // assert the type of ev.target            
+              db = request.result;    // access the database instance
+
+              if(!db.objectStoreNames.contains('csvData_os')){
+                  db.createObjectStore('csvData_os', { keyPath: "Role" });    // "{ autoIncrement: true }" - every record stored in the csvData_os object store will be automatically assigned a unique numeric key, starting from 1, and incrementing with each new entry.
+              }
+
+          });
+
+          // success handler
+          openRequestCSVdb.addEventListener("success", (ev: Event) => {
+              const request = ev.target as IDBOpenDBRequest;
+              
+              resolve(request.result);
+              console.log("CSV database, opened successfully");
+          });
+
+          // error handler
+          openRequestCSVdb.addEventListener("error", (ev: Event) => {
+              const request = ev.target as IDBOpenDBRequest; 
+              
+              reject(request.error);
+              console.log("CSV database failed to open");
+          });
+      });
+
+      return initIDB;
+  };
+
+    // Add CSV JSON Data into IDB
+  async storeCSVJSONDataInIDB(csvData: any): Promise<IDBDatabase> { // we use "<IDBDatabase>" and resolve(db) becouse this method is for STORING and UPDATING
+
+      return this.initCSVIndexedDB().then((db: IDBDatabase) => {
+          const storeCSVJSONDataInIDB = new Promise<IDBDatabase>((resolve, reject) => {
+
+              // open a read/write db transaction, ready for adding the data
+              const CSVTransaction = db.transaction(['csvData_os'], "readwrite");
+
+              // create a "CSVObjectsStore" variable to enable operations such as: adding, retrieving(reading), updating, deleting records within this "object store" during the scope of transaction
+              const CSVObjectsStore = CSVTransaction.objectStore('csvData_os');
+
+              // check if the data is an array or a single object and convert to an array if needed (later implemented). We achieved this using "Array" class to convert anything into Array and checking that condition using "Ternary" operator
+              const dataArray = Array.isArray(csvData) ? csvData : [csvData];         // this ensures that dataArray is always an array, whether the input is a single object or an array of objects.
+                                                                                      // because at "Add / Edit Job Employer" we modify the job (if wanted to) ONLY if we pressing "Manage" anchor button at "Main View Employer" page
+
+              dataArray.forEach(item => {      // before was "csvData.forEach()"
+                  CSVObjectsStore.put(item);   // add - if we want add ONLY and for update we need to use "ClearCsvIDBStore" method in "Submit" button in try catch block
+                                              // put - if updates a given record in a database, or inserts a new record if the given item does not already exist. NO "ClearCsvIDBStore" is needed any more
+              });
+
+              CSVTransaction.oncomplete = () => {
+                      
+                  resolve(db);    // Explicitly resolve with the IDBDatabase instance
+                  console.log("Transaction completed successfully");
+                  console.log("CSV file added to database table successfully");
+              };
+                                                  
+              CSVTransaction.onerror = (ev: Event) => {
+                  reject(CSVTransaction.error);   // Reject with the transaction error
+                  console.error("CSV file is NOT added to database table");
+                  console.error("Transaction failed");
+              };
+
+          });
+
+          return storeCSVJSONDataInIDB;
+      });
+  };
 }
