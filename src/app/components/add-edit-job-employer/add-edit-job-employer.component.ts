6642ae4ca876238654285149da3bc80c74ca2535
Chris2240 <kris2240.kp@gmail.com>
2025-08-05 20:33:28 +0100

    Add-edit-job-employer component - Providing employer name and picture from localStorage

--- src/app/components/add-edit-job-employer/add-edit-job-employer.component.ts
+++ src/app/components/add-edit-job-employer/add-edit-job-employer.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-add-edit-job-employer',
   templateUrl: './add-edit-job-employer.component.html',
   styleUrl: './add-edit-job-employer.component.css'
 })
-export class AddEditJobEmployerComponent {
+export class AddEditJobEmployerComponent implements OnInit, AfterViewInit {
 
+  // binding elements using properties and "@ViewChild" decorator properties
+  spanEmployerName = '';
+  
+  @ViewChild('imgElement') imgElementRef! : ElementRef<HTMLImageElement>;
+  
+  
+  constructor(){}
+
+  
+  ngOnInit(): void {
+    
+    this.getEmployerNameFromLS();
+  }
+
+  ngAfterViewInit(): void {
+    
+    this.getEmployerPictureFromLS();
+  }
+
+  
+  // all methods: --------------------------------
+
+  getEmployerNameFromLS(): void{
+    const savedEmployerName = localStorage.getItem('employer-profile-employer-name') || 'Employer Name';
+
+    if (savedEmployerName){      
+      this.spanEmployerName = savedEmployerName.trim();
+    }
+  }
+
+  getEmployerPictureFromLS(): void{
+    const savedImage = localStorage.getItem('employer-profile-picture-src');
+    const imgElement = this.imgElementRef.nativeElement;
+
+    if(savedImage){
+      imgElement.src = savedImage;
+    }
+  }
 }
